a240d10180c582e05cd2eae1371c199ed11daff3
Castle78 <castillobal@gmail.com>
2014-02-01 17:01:41 -0600

    Hey!!

---
+++ proyectoGIH/src/proyectoGIH/ClaveSAM.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package proyectoGIH;
+
+/**
+ * @author MAC_ISRA
+ *
+ */
+public class ClaveSAM {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pinche SAM");
+	}
+
+}
